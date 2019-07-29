54df567fe89db4475e9491e146f782cc24d3f3bf
github username <github email address>
2019-07-29 17:14:41 -0500

    slang list done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Home = () => {
- return (
-   <div>
-     <h1>Slang List</h1>
-   </div>
- );
+
+const API = 'https://slang-challenge-backend.herokuapp.com/cards';
+
+export default class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      slangs: [],
+    }
+  }
+  componentDidMount = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(slangs => this.setState({
+      slangs:  slangs.sort((a, b) => (a.phrase > b.phrase) ? 1 : -1)
+    }))
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>Slang List</h1>
+
+         {this.state.slangs.map(slang => <li>{slang.phrase}: {slang.acronym}</li>)}
+
+      </div>
+    );
+  }
 };
-
-export default Home;
